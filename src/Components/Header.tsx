ba8beb461eb1fd76997eb4ba0194a15d7eca2c88
ShuVeriDA <bashtarov@outlook.com>
2022-03-01 21:19:46 +0300

    12. breakdown into components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://cryptologos.cc/logos/aave-aave-logo.png" alt=""/>
+        </header>
+    );
+};
+
+export {Header};
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/4f6DfV5DbqaQUSw0uo0mWi/6fbcf889bdef65c5b92ffee86b13fc44/shutterstock_376532611.jpg?fit=fill&w=800&h=300" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                Mypost
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export {Profile};
