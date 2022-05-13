5f7d99c217ca8e61ca5075abdd425dae57103ed1
ShuVeriDA <bashtarov@outlook.com>
2022-05-13 16:32:08 +0300

    60 withRouter, props.match.params

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@ function App(props: AppType) {
          <NavBar/>
          <div className='app-wrapper-content'>
             <Routes>
-               <Route path="/profile" element={<ProfileContainer />}/>
+               <Route path='/profile/:userId' element={<ProfileContainer/>}/>
+               <Route path='/profile' element={<ProfileContainer />} />
                <Route path="/dialogs" element={<DialogsContainer />}/>
                <Route path="/users" element={<UsersContainer />}/>
             </Routes>
--- src/Components/Profile/ProfileContainer.tsx
+++ src/Components/Profile/ProfileContainer.tsx
@@ -3,6 +3,7 @@ import {Profile} from "./Profile";
 import axios from "axios";
 import {connect} from "react-redux";
 import {ProfilePageType, setUserProfileAC} from "../../redux/profileReducer";
+import {useLocation, useMatch, useNavigate, useParams} from "react-router-dom";
 
 export type MapStateToProps = {
    profilePage: ProfilePageType
@@ -11,11 +12,25 @@ export type MapStateToProps = {
 export type ProfileContainerType = {
    profile: null
    setUserProfileAC: (profile: null) => void
+   router: any
 }
+
+export const withRouter = (WrappedComponent: typeof React.Component) => {
+   return (props: object) => {
+
+      const params = useParams(); //useParams возвращает объект пары key/value (ключ/значение) параметров URL.
+
+      return (
+         <WrappedComponent {...props} router={params}/>
+      );
+   }
+}
+
 
 class ProfileContainer extends React.Component<ProfileContainerType, any> {
    componentDidMount() {
-      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      let userId = this.props.router.userId || 2
+      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
          .then(response => {
             this.props.setUserProfileAC(response.data)
          })
@@ -32,4 +47,4 @@ let mapStateToProps = (state: MapStateToProps) => ({
    profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfileAC})(ProfileContainer)
+export default connect(mapStateToProps, {setUserProfileAC})(withRouter(ProfileContainer))
